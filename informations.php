e969e232f7e6d50b22660f4f3877ed139a1d4235
Unknown <b.m78990@gmail.com>
2018-07-27 20:56:07 +0200

    Add informations.php

--- informations.php
+++ informations.php
@@ -3,14 +3,30 @@ session_start();
 
 $title = 'ZelEnjoy - Informations';
 $navPage = 'Informations';
+
 ?>
 <html lang="fr" dir="ltr">
   <head>
-    <?php include "./includes/head.php"?>
+  <?php include "./includes/head.php"?>
   </head>
-  <body>
+  <body class="toggler main">
     <div class="website">
       <?php include "./includes/navbar.php";?>
+      
     </div>
+
+    <?php include "./includes/chat.php"; ?>
+
+    <div class="frame-stream-mini-player">
+      <!-- CHANNEL -->
+      <iframe src="<?php echo "https://player.twitch.tv/?channel={$channel}"; ?>"
+              width="100%"
+              height="100%"
+              autoplay=""
+              allowfullscreen=""
+              frameborder="0">
+      </iframe>
+    </div>
+    <script src="js/app.js"></script>
   </body>
 </html>
